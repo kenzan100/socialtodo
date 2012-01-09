ded1aa5aa11958083d3ee3ddcf34303ad2041ab5
camelmasa <camelmasa@gmail.com>
2012-01-10 07:01:39 +0900

    update file

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -112,7 +112,7 @@ class TasksController < ApplicationController
     #TODO double remote call
     
     api = Koala::Facebook::API.new(current_user.token)
-    api.put_connections("me", ENV['NAME_SPACE']+":"+ENV['OBJECT_TYPE'], :task => 'http://socialtodo.herokuapp.com/tasks/'+params[:id])
+    api.put_connections("me", ENV['NAME_SPACE']+":"+ENV['ACTION_TYPE'], :task => 'http://socialtodo.herokuapp.com/tasks/'+params[:id])
     render
   end
 
