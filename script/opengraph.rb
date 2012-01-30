7989fa125c0d36a00997518e60d5080dd9e8d474
camelmasa <camelmasa@gmail.com>
2012-01-31 05:59:54 +0900

    bug fix

--- script/opengraph.rb
+++ script/opengraph.rb
@@ -2,7 +2,7 @@
 
 Job.all.each do |j|
   api = Koala::Facebook::API.new(j.user.token)
-  api.put_connections("me", ENV['NAME_SPACE']+":"+ENV['ACTION_TYPE'], :task => 'http://socialtodo.herokuapp.com/tasks/'+j.task_id)
+  api.put_connections("me", ENV['NAME_SPACE']+":"+ENV['ACTION_TYPE'], :task => 'http://socialtodo.herokuapp.com/tasks/'+j.task_id.to_s)
 end
 
 Job.delete_all
